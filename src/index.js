da313a257f547be0c1af378df100a12cc4a98e8d
Gilles Coomans <gilles.coomans@gmail.com>
2017-04-16 16:45:11 +0200

    fix(code): import code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,72 @@
-export default class Dummy {
-	constructor (name='batman') {
-		this.name = name;
-	}
+/**
+ * @author Gilles Coomans
+ * @licence MIT
+ * @copyright 2016 Gilles Coomans
+ */
 
-	helloMessage () {
-		const msg = `hello ${this.name}`;
-		return msg;
-	}
+const babelute = require('babelute');
 
-	sayHello () {
-		console.log(this.helloMessage()); // eslint-disable-line no-console
-	}
-}
+const arightLexicon = babelute.createLexicon('aright');
+
+arightLexicon
+	.addAtoms([
+		'is',
+		'has',
+		'strict',
+		'not',
+		'switch',
+		'required',
+		'minLength',
+		'maxLength',
+		'minimum',
+		'maximum',
+		'format',
+		'enum',
+		'item',
+		'equal',
+		'instanceOf',
+		'type',
+		'array',
+		'isArray',
+		'null'
+	])
+	.addCompounds(() => {
+		return {
+			between(min, max) {
+				return this.minimum(min).maximum(max);
+			},
+			isNull() {
+				return this.equal(null);
+			},
+			or() {
+				return this._append('aright', 'or', [].slice.call(arguments));
+			},
+			required(yes) {
+				this._aright_required = !!yes;
+				return this;
+			},
+			email() {
+				return this.isString().format('email').minLength(6);
+			}
+		};
+	})
+	.addCompounds(() => {
+		const methods = {};
+		['object', 'string', 'func', 'bool', 'number', 'boolean']
+		.forEach((type) => {
+			methods['is' + type[0].toUpperCase() + type.substring(1)] = function() {
+				return this.is(type);
+			};
+			methods[type] = function(name, babelute) {
+				return this.has(name, type, babelute);
+			};
+		});
+		return methods;
+	});
+
+
+
+export default arightLexicon;
+
+//______________________________________________________
+
